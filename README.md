57c837cea17fa162bcdb17fd072840d6abf49f1e
Pierre-Emmanuel Vilcot <pevilcot@gmail.com>
2022-02-15 22:47:34 +0100

    Structure de base

---
+++ README.md
@@ -0,0 +1,4 @@
+# Digiday
+
+Partie back-end du projet de gestion des congés.
+Fonctionne grâce à [Spring Boot](https://spring.io/projects).
---
+++ src/main/java/fr/diginamic/digiday/DigidayApplication.java
@@ -0,0 +1,13 @@
+package fr.diginamic.digiday;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DigidayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DigidayApplication.class, args);
+	}
+
+}
