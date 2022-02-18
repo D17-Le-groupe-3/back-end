667c05e22ac2a0c2d30989b3acccac5d3dbbcc83
Pierre-Emmanuel Vilcot <pevilcot@gmail.com>
2022-02-18 13:07:54 +0100

    security : allow all

--- src/main/java/fr/diginamic/digiday/DigidayApplication.java
+++ src/main/java/fr/diginamic/digiday/DigidayApplication.java
@@ -2,34 +2,16 @@ package fr.diginamic.digiday;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = SecurityAutoConfiguration.class)
 public class DigidayApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DigidayApplication.class, args);
-	}
-
-	@Configuration
-	protected static class SecurityConfiguration extends WebSecurityConfigurerAdapter {
-		@Override
-		protected void configure(HttpSecurity http) throws Exception {
-			http
-					.httpBasic()
-					.and()
-					.authorizeRequests()
-					.antMatchers("/index.html", "/", "/home", "/login").permitAll()
-					.anyRequest().authenticated()
-					.and().csrf()
-					.csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse());;
-		}
 	}
 
 	@Bean
