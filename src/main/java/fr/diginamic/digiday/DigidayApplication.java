1419820d216ea767c61bf241dacbdd09216eef41
Pierre-Emmanuel <pevilcot@gmail.com>
2022-02-18 13:42:12 +0100

    Config basique de la sécurité pour dev (#18)
    close #2 
    security : allow all

--- src/main/java/fr/diginamic/digiday/DigidayApplication.java
+++ src/main/java/fr/diginamic/digiday/DigidayApplication.java
@@ -2,12 +2,25 @@ package fr.diginamic.digiday;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = SecurityAutoConfiguration.class)
 public class DigidayApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DigidayApplication.class, args);
 	}
 
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**").allowedOrigins("http://localhost:4200");
+			}
+		};
+	}
 }
