3ebd6dbb74319dfc978243c43b65784142e63879
Pierre-Emmanuel Vilcot <pevilcot@gmail.com>
2022-02-24 14:40:22 +0100

    enhanced spring security

--- src/main/java/fr/diginamic/digiday/SecurityConfig.java
+++ src/main/java/fr/diginamic/digiday/SecurityConfig.java
@@ -1,15 +1,46 @@
 package fr.diginamic.digiday;
 
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+@Configuration
+@EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http
-                .cors()
-                .and()
-                .authorizeRequests()
-                .antMatchers("/**").permitAll();
+        /*
+         * Attention je désactive SpringBootSecurity
+         */
+        http.csrf().disable().
+                authorizeRequests().anyRequest().
+                permitAll().and().httpBasic();
+
+        /*
+         * Site inacessible :)
+         * Pas de login rien :)))
+         * http.csrf().disable().
+         authorizeRequests().anyRequest().
+         authenticated();
+         */
+		/*
+		http.csrf().disable()
+		.formLogin().
+		loginProcessingUrl("/login").and()
+		.logout().logoutUrl("/logout").
+		invalidateHttpSession(true).and()
+		.authorizeRequests()
+		.antMatchers("/login").permitAll()
+		.antMatchers("/logout").permitAll()
+		.anyRequest().authenticated().and().
+		httpBasic();*/
+        /*
+         * httpBasic() -> API REST
+         * avec leur propre sécurité
+         */
     }
 }
